df0959091e5c271b4a1794184506b01713bf7fd4
Kevin Harrington <mad.hephaestus@gmail.com>
2018-07-18 16:10:23 -0400

    Create dShaft.groovy

---
+++ dShaft.groovy
@@ -0,0 +1,21 @@
+
+if(args == null)
+	args = [6.0,5.4,11]
+
+LengthParameter printerOffset = new LengthParameter("printerOffset",0.25,[2,0.001])
+
+
+double finalp = args.get(0)/2+(printerOffset.getMM()/2)
+
+double finalf = args.get(1)+(printerOffset.getMM()/2)
+
+CSG pshaft =new Cylinder(finalp,finalp,args.get(2),(int)30).toCSG() // a one line Cylinder
+CSG flat = new Cube(	finalp*2,// X dimention
+			finalf,// Y dimention
+			args.get(2)//  Z dimention
+			).toCSG()
+			.toYMin()
+			.toZMin()
+			.movey(pshaft.getMinY())
+
+return pshaft.intersect(flat)
